f6a76ab81eaba5d32dc90759d1a9bf33fd652e5e
zhouxinghe <39587989+zhouxinghe@users.noreply.github.com>
2018-05-30 19:25:12 +0800

    update config
    1.change pool_reward to 1%.
    2.change pubkey to BAFptCxbAVTDkG+80193FZ7GkFPDJKw0ofsrEZmUSwD8O3fFIRtJMW4NKdWSRELpvprz+Bk8HlVNCDFAj2dywbY=
    3.chaneg payout_limit to 0.2veo

--- apps/amoveo_mining_pool/src/config.erl
+++ apps/amoveo_mining_pool/src/config.erl
@@ -8,9 +8,9 @@ external() ->
     Y = lists:reverse(X),
     Z = [hd("/")|[hd("0")|tl(tl(Y))]],
     lists:reverse(Z).
-pool_reward() -> {0, 100}.%this is the portion of the block reward that goes to the mining pool. It is a fraction {numerator, denominator}. for example {1, 9} would mean that 1/9th of the block reward is kept as a fee, and 8/9ths are paid to miners..
+pool_reward() -> {1, 100}.%this is the portion of the block reward that goes to the mining pool. It is a fraction {numerator, denominator}. for example {1, 9} would mean that 1/9th of the block reward is kept as a fee, and 8/9ths are paid to miners..
 block_reward() -> 100227592.
-pubkey() -> "BCjdlkTKyFh7BBx4grLUGFJCedmzo4e0XT1KJtbSwq5vCJHrPltHATB+maZ+Pncjnfvt9CsCcI9Rn1vO+fPLIV4=". %Initially, this pubkey controls all the shares in the pool. About half of the first (rt() + 1) block rewards will go to this account. This is important so that we don't over-reward the miners of the first 10 blocks.
+pubkey() -> "BAFptCxbAVTDkG+80193FZ7GkFPDJKw0ofsrEZmUSwD8O3fFIRtJMW4NKdWSRELpvprz+Bk8HlVNCDFAj2dywbY=". %Initially, this pubkey controls all the shares in the pool. About half of the first (rt() + 1) block rewards will go to this account. This is important so that we don't over-reward the miners of the first 10 blocks.
 %When you are ready to shut off your node, first do `accounts:final_reward().` this way you don't under-reward the miners of the last 10 blocks. Use the extra money you got from the first 10 blocks to afford to pay the miners of the last 10 blocks.
 %share_block_ratio() -> 1.%useful for testing.
 share_block_ratio() -> 11.% for every block, we pay out 2^share_block_ratio many rewards.
@@ -18,7 +18,7 @@ share_block_ratio() -> 11.% for every block, we pay out 2^share_block_ratio many
 rt() -> 9.%rewards are smoothed out over the last rt()+1 blocks.
 ratio() -> {rt(), rt()+1}.
 tx_fee() -> 152000.%when you shut off the pool, it pays out to everyone who has more than this much veo.
-payout_limit() -> 40000000.%when a miner has more than this much veo, it automatically pays out to you.
+payout_limit() -> 20000000.%when a miner has more than this much veo, it automatically pays out to you.
 refresh_period() -> 2.%how often we get a new problem from the server to work on. in seconds
 confirmations() -> 5. %how many confirmations does a block need before we can pay out the reward
 spend_log_file() -> "spend.log".
